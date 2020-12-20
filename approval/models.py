18d0107cd68c240c7730c19828bddb3c0b88fb76
yejun1060 <yejun1060@daum.net>
2020-12-20 17:59:58 +0900

    . Add Model (have error) [ approval > models ]

--- approval/models.py
+++ approval/models.py
@@ -1,23 +1,52 @@
 from django.db import models
+from datetime import datetime
+import sys
+from os import path
 
 
-class Paper(models.Model):
-    title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField('date published') #create date
-    drafter = models.CharField(max_length=20) #name
-    status = models.CharField(max_length=8, default='기안')
+# Fix Model Import Error
+if __name__ == '__main__':
+    if __package__ is None:
+        sys.path.append(path.dirname(path.dirname( path.abspath(__file__))))
+        from ..member.models import User
+    else:
+        from ..member.models import User
 
+
+class Docs(models.Model):
+    # Basic Information
+    title = models.CharField(max_length=30)
+
+    # Other Informations
+    pub_date = models.DateTimeField('date published', default=datetime.now)
+    score = models.CharField(max_length=1, default='0')
+
+    # output
     def __str__(self):
-        return self.title
+        response = self.title + "(" + self.score + ")"
+        return response
 
 
-class Related_person(models.Model):
-    paper = models.ForeignKey(Paper, on_delete=models.CASCADE) #paper id
-    first = models. CharField(max_length=10) #name
-    second = models.CharField(max_length=10) #name
-    third = models.CharField(max_length=10) #name
-    four = models.CharField(max_length=10) #name
+class Docs_advanced(models.Model):
+    # Basic Information
+    docs_id = models.ForeignKey(Docs, on_delete=models.PROTECT)
+    drafter = models.ForeignKey(User, on_delete=models.PROTECT)
 
+    # Signed Information
+    stf1 = models.ForeignKey(User, on_delete=models.PROTECT)
+    stf1_date = models.DateTimeField('date signed', related_name='first_date')
+
+    stf2 = models.ForeignKey(User, on_delete=models.PROTECT)
+    stf2_date = models.DateTimeField('date signed', related_name='second_date')
+
+    stf3 = models.ForeignKey(User, on_delete=models.PROTECT)
+    stf3_date = models.DateTimeField('date signed', related_name='third_date')
+
+    # Other Informations
+    reason = models.CharField(max_length=50)
+    status = models.CharField(max_length=5, default='기안')
+
+    # output
     def __str__(self):
-        response = "(" + self.paper.title + ")의 건에 관한 결재자"
+        response = self.drafter + "(" + self.status + ")"
         return response
--- member/admin.py
+++ member/admin.py
@@ -9,6 +9,9 @@ class AuthInfoAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['created_date']}),
         ('Valid Information (0: false, 1: true(default))', {'fields': ['is_valid']}),
     ]
+    list_display = ('id', 'name', 'created_date')
+    list_filter = ['is_valid']
+    search_fields = ['name']
 
 
 # separate filed
@@ -20,7 +23,9 @@ class UserAdmin(admin.ModelAdmin):
         ('other Informations - 1 (0: false, 1: true(default))', {'fields': ['is_active']}),
         ('other Informations - 2 (0: false(default), 1: true)', {'fields': ['is_staff', 'is_superuser']}),
     ]
-    list_display = ('last_name', 'first_name', 'joined_date', 'is_staff')
+    list_display = ('id', 'last_name', 'first_name', 'joined_date', 'is_staff')
+    list_filter = ['joined_date', 'is_active', 'is_staff']
+    search_fields = ['nickname', 'last_name']
 
 
 # separate filed
@@ -30,8 +35,8 @@ class UserAuthAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['given_date', 'modified_date']}),
         ('Valid Information (0: false, 1: true(default))', {'fields': ['is_valid', 'authorization']}),
     ]
-    list_display = ('UserId', 'authorization', 'modified_date', 'AuthId')
-    list_filter = ['authorization']
+    list_display = ('id', 'UserId', 'authorization', 'modified_date', 'AuthId')
+    list_filter = ['authorization', 'modified_date']
 
 
 # add admin site
